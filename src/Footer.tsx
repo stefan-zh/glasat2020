486c9194238e916ae983f6ce94ec8a5642d5fe82
Stefan Zhelyazkov <stefan.zhelyazkov@gmail.com>
2022-01-15 16:57:00 +0200

    Replace MaterialUI in Footer with regular HTML and CSS

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,26 +1,9 @@
 import * as React from 'react';
-import { makeStyles, Typography, Link } from '@material-ui/core';
 
-const useStyles = makeStyles({
-  footer: {
-    margin: '60px 0 30px'
-  },
-});
-
-export const Footer = () => {
-  const classes = useStyles();
-
-  return (
-    <div className={classes.footer}>
-      <Typography variant="subtitle1" color="textSecondary">
-        © 2020 Автор <Link href="https://www.stefanzh.com" variant="subtitle1" color="inherit" underline="always" target="_blank">Стефан Желязков</Link>.
-      </Typography>
-      <Typography variant="subtitle1" color="textSecondary">
-        Гласът на България е запазена търговска марка на "БТВ Медиа Груп" ЕAД.
-      </Typography>
-      <Typography variant="subtitle1" color="textSecondary">
-        YouTube е платформа за споделяне на видео клипове и е запазена търговска марка на Google LLC.
-      </Typography>
-    </div>
-  );
-}
+export const Footer = () => (
+  <footer className="secondary">
+    © 2020 Автор <a href="https://www.stefanzh.com" target="_blank">Стефан Желязков</a>.<br/>
+    Гласът на България е запазена търговска марка на "БТВ Медиа Груп" ЕAД.<br />
+    YouTube е платформа за споделяне на видео клипове и е запазена търговска марка на Google LLC.
+  </footer>
+);
